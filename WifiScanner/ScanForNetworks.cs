794edb35ce156e4bc420d476d93ce57401480962
Ryan <31938131+rytrotter@users.noreply.github.com>
2019-04-01 15:38:36 -0400

    Add files via upload

---
+++ WifiScanner/ScanForNetworks.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NativeWifi;
+
+namespace WifiAdminTools
+{
+    class ScanForNetworks
+    {
+
+        public static List<Networking.NetworkInfo> GatherScannedNetworkInfo()
+        {
+            List<Networking.NetworkInfo> listOfNetworks = new List<Networking.NetworkInfo>();
+            
+            // Gathers interfaces to use
+            foreach ( WlanClient.WlanInterface wlanInterface in Networking.client.Interfaces )
+            {
+                Wlan.WlanAvailableNetwork[] scannedNetworks = wlanInterface.GetAvailableNetworkList(Wlan.WlanGetAvailableNetworkFlags.IncludeAllManualHiddenProfiles);
+                // Goes through each networkIndex it sees to parse information
+                foreach (Wlan.WlanAvailableNetwork networkIndex in scannedNetworks)
+                {
+                    Networking.NetworkInfo networkInfo = new Networking.NetworkInfo();
+                    Wlan.Dot11Ssid ssid = networkIndex.dot11Ssid;
+                    string networkName = "";
+
+                    if ( string.IsNullOrEmpty(Encoding.ASCII.GetString(ssid.SSID, 0, (int)ssid.SSIDLength) ) || Encoding.ASCII.GetString(ssid.SSID, 0, (int)ssid.SSIDLength) == " ")
+                    {
+                        networkName = "*Hidden Network*";
+                    }
+                    else
+                    {
+                        networkName = Encoding.ASCII.GetString(ssid.SSID, 0, (int)ssid.SSIDLength);
+                    }
+
+                    string networkSecurity = networkIndex.dot11DefaultCipherAlgorithm.ToString();
+                    string networkSignalStrength = networkIndex.wlanSignalQuality.ToString();
+
+                    listOfNetworks.Add(new Networking.NetworkInfo
+                        {
+                            SSID = networkName,
+                            SecurityType = networkSecurity,
+                            SignalStrength = networkSignalStrength
+                        });
+                }
+                
+            }
+            
+            return listOfNetworks;
+        }
+    }
+}
